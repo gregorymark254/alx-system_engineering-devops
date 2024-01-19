ed3aeff2b6ce485659a0a6b09289cab49692374a
gregorymark254 <gregorymark254@gmail.com>
2024-01-20 00:16:37 +0300

    puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,4 @@
-exec { 'killmenow_process':
-  command     => '/usr/bin/pkill -f killmenow',
-  refreshonly => true,
-  subscribe   => File['/tmp/start_killmenow_process'],
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
